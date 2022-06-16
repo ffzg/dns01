93efb0685bc2c90b21959302a4091617af75daa9
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-06-16 09:43:24 +0200

    dump parsed bind config

---
+++ bind-config.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use warnings;
+use strict;
+
+use Data::Dump qw(dump);
+
+use lib './lib';
+use BIND::Config qw( check_config @zones );
+
+my $debug = $ENV{DEBUG} || 0;
+
+check_config( $ARGV[0] || "/etc/bind/named.conf" );
+
+print "zones = ",dump( @zones );
+
