317bb453f06f4827fabc0ee828def4fa0b5bb4f4
Dobrica Pavlinusic <dpavlin@rot13.org>
2022-06-16 09:26:16 +0200

    extract bind config parser into BIND::Config

--- bind-lint.pl
+++ bind-lint.pl
@@ -2,53 +2,10 @@
 use warnings;
 use strict;
 
-# sudo apt install libparse-recdescent-perl
+use lib './lib';
+use BIND::Config qw( check_config @zones );
 
 my $debug = $ENV{DEBUG} || 0;
-
-use lib './lib';
-use BIND::Config::Parser;
-
-my @zones;
-
-sub check_config {
-	my ( $config_file ) = @_;
-	warn "# check_config $config_file\n" if $debug;
-
-	my $parser = new BIND::Config::Parser;
-	 
-	my $indent = 0;
-	my $zone;
-	 
-	# Set up callback handlers
-	$parser->set_open_block_handler( sub {
-		print "\t" x $indent, join( " ", @_ ), " {\n" if $debug;
-		$indent++;
-		if ( $_[0] eq 'zone' ) {
-			$zone = $_[1];
-		}
-	} );
-	$parser->set_close_block_handler( sub {
-		$indent--;
-		print "\t" x $indent, "};\n" if $debug;
-	} );
-	$parser->set_statement_handler( sub {
-		print "\t" x $indent, join( " ", @_ ), ";\n" if $debug;
-		if ( $_[0] eq 'file' ) {
-			push @zones, [ $zone, $_[1] ];
-		}
-		if ( $_[0] eq 'include' ) {
-			my $file = $_[1];
-			$file =~ s/^"//;
-			$file =~ s/"$//;
-			check_config( $file );
-		}
-	} );
-
-	# Parse the file
-	$parser->parse_file( $config_file );
-
-}
 
 check_config( $ARGV[0] || "/etc/bind/named.conf" );
 
---
+++ lib/BIND/Config.pm
@@ -0,0 +1,55 @@
+package BIND::Config;
+use warnings;
+use strict;
+
+our @ISA = qw( Exporter );
+our @EXPORT = qw( check_config @zones );
+
+# sudo apt install libparse-recdescent-perl
+
+my $debug = $ENV{DEBUG} || 0;
+
+use BIND::Config::Parser;
+
+our @zones;
+
+sub check_config {
+	my ( $config_file ) = @_;
+	warn "# check_config $config_file\n" if $debug;
+
+	my $parser = new BIND::Config::Parser;
+	 
+	my $indent = 0;
+	my $zone;
+	 
+	# Set up callback handlers
+	$parser->set_open_block_handler( sub {
+		print "\t" x $indent, join( " ", @_ ), " {\n" if $debug;
+		$indent++;
+		if ( $_[0] eq 'zone' ) {
+			$zone = $_[1];
+		}
+	} );
+	$parser->set_close_block_handler( sub {
+		$indent--;
+		print "\t" x $indent, "};\n" if $debug;
+	} );
+	$parser->set_statement_handler( sub {
+		print "\t" x $indent, join( " ", @_ ), ";\n" if $debug;
+		if ( $_[0] eq 'file' ) {
+			push @zones, [ $zone, $_[1] ];
+		}
+		if ( $_[0] eq 'include' ) {
+			my $file = $_[1];
+			$file =~ s/^"//;
+			$file =~ s/"$//;
+			check_config( $file );
+		}
+	} );
+
+	# Parse the file
+	$parser->parse_file( $config_file );
+
+}
+
+1;
