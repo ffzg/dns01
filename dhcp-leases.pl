307cb11b43de22a2f6a4aa00518d0d5dc2c79de3
FFZG admin <admin@ffzg.hr>
2023-11-19 13:34:35 +0100

    added duration of lease

--- dhcp-leases.pl
+++ dhcp-leases.pl
@@ -7,6 +7,7 @@ use Data::Dump qw(dump);
 
 use lib './lib';
 use DHCPD::Leases qw(parse_leases);
+use HTTP::Date;
 
 my $debug = $ENV{DEBUG} || 0;
 
@@ -22,11 +23,17 @@ foreach my $ip ( sort keys %$lease ) {
 
 	next if ( ! @ARGV && $data->{'binding state'} ne 'active' ); # FIXME
 	warn "# ",dump($data) if $debug;
+	my $starts_t = str2time $data->{'starts'};
+	my $ends_t = str2time $data->{'ends'};
+	my $d_t = $ends_t - $starts_t;
+	$data->{d_t} = $d_t;
+	print "XX lease time $d_t ",dump($data) if $debug && $d_t > 600;
 	print join(' ', map { defined $data->{$_} ? $data->{$_} : '?' } (
 				'lease',
 				'hardware ethernet',
 				'binding state',
 				'ends',
+				'd_t',
 				'client-hostname',
 				'ddns-fwd-name',
 				'ddns-rev-name',
