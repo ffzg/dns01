4c94020f231e8e7b559b1cdaf8d7d854f1a7dd21
Dobrica Pavlinusic <dpavlin@rot13.org>
2024-01-27 15:11:56 +0100

    report ips by subnet defined in dhcpd.conf and includes

--- dhcp-leases.pl
+++ dhcp-leases.pl
@@ -9,6 +9,8 @@ use lib './lib';
 use DHCPD::Leases qw(parse_leases);
 use HTTP::Date;
 use Getopt::Long;
+use Net::IP;
+use Net::Netmask;
 
 my $debug = $ENV{DEBUG} || 0;
 my $all;
@@ -25,14 +27,91 @@ GetOptions(
 $|=1 if $debug;
 
 my $stat;
+
+my $CONFIGFILE = '/etc/dhcp/dhcpd.conf';
+parseconfig($CONFIGFILE);
+
+# isc-dhcp-server config parser from munin plugin dhcpd3
+
+my @netmasks;
+
+sub parseconfig {
+    my($configfile) = @_;
+
+    warn "# parseconfig ", $configfile;
+    my $DEBUG = 1;
+    my %limits;
+
+    local(*IN);
+    open(IN, "<$configfile") or exit 4;
+
+    my $name = undef;
+    LINE: while(<IN>) {
+	if(/subnet\s+((?:\d+\.){3}\d+)\s+netmask\s+((?:\d+\.){3}\d+)/ && ! /^\s*#/) {
+	    $name = "$1 $2";
+	    print "# DEBUG: Found a subnet: $name\n" if $DEBUG;
+	    $stat->{subnet}->{$1} = $2;
+	    push @netmasks, Net::Netmask->new( $1, $2 );
+	}
+	if($name && /^\}$/) {
+            if(!exists $limits{$name}) {
+                print "# DEBUG: End of subnet... NO RANGE?\n" if $DEBUG;
+            }
+	    $name = "";
+	}
+	if($name && /range\s+((?:\d+\.){3}\d+)\s+((?:\d+\.){3}\d+)/) {
+	    print "# DEBUG: range $1 -> $2\n" if $DEBUG;
+	    $limits{$name} += &rangecount($1, $2);
+	    print "# DEBUG: limit for $name is " . $limits{$name} . "\n" if $DEBUG;
+	}
+	if(/^include \"([^\"]+)\";/) {
+	    my $includefile = $1;
+	    print "# DEBUG: found included file: $includefile\n" if $DEBUG;
+	    if(!-f $includefile) {
+		$includefile = dirname($CONFIGFILE) . "/" . $includefile;
+		if(!-f $includefile) {
+		    next LINE;
+		}
+	    }
+	    parseconfig($includefile);
+	}
+    }
+    close(IN);
+}
+
+warn "# netmasks ", dump( \@netmasks ) if $debug;
+
+sub rangecount {
+    my ($from, $to) = @_;
+
+    $from = ((new Net::IP($from))->intip())->numify();
+    $to   = ((new Net::IP($to))->intip())->numify();
+    
+    if($from < $to) {
+	return ($to - $from) + 1;
+    } else {
+	return ($from - $to) + 1;
+    }
+}
+
+
+
 my $lease = parse_leases( '/var/lib/dhcp/dhcpd.leases' );
 
 foreach my $ip ( sort keys %$lease ) {
 	my $data = $lease->{$ip};
 
 	$stat->{ $data->{'binding state'} }++;
+	foreach my $netmask ( @netmasks ) {
+		if ( $netmask->match( $ip ) ) {
+			$stat->{ 'ips2' }->{$netmask}->{ $data->{'binding state'} }++;
+			$stat->{ 'ips2' }->{$netmask}->{ _total }++;
+		}
+	}
+
 	my $ip_24 = $ip; $ip_24 =~ s/\.\d+$//;
 	$stat->{ 'ips' }->{$ip_24}->{ $data->{'binding state'} }++;
+	$stat->{ 'ips' }->{$ip_24}->{ _total }++;
 
 	next if ( ! $long && ! $all && $data->{'binding state'} ne 'active' ); # FIXME
 	warn "# ",dump($data) if $debug;
